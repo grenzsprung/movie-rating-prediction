f03ce25372fe0f64c8c2eece123452b64e57738a
Tim R. Schleicher <55151331+grenzsprung@users.noreply.github.com>
2019-11-08 01:41:37 +0100

    Update user_movies_matrix.R

--- doc/user_movies_matrix.R
+++ doc/user_movies_matrix.R
@@ -1,18 +1,13 @@
 library(dplyr)
 library(tidyr)
 
-setwd("/Users/timschleicher/Documents/GitHub/fall2019-project4-sec2-grp8")
-
 # Load ratings data
-
-ratings <- read.csv("data/ml-latest-small/ratings.csv")
+ratings <- read.csv("./data/ml-latest-small/ratings.csv")
 
 # Create user x movie matrix with ratings
-
 ratings <- ratings %>%
   tbl_df() %>% select(userId, movieId, rating) %>%
   spread(movieId, rating)
 
 # Save matrix
-
-save(ratings, file = "output/ratings_matrix.Rdata")
+save(ratings, file = "./output/ratings_matrix.Rdata")
